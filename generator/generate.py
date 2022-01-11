56dbb92535dbfae4f91f2726f0efe870bdff5120
Christian Nilsson <nikize@gmail.com>
2022-01-11 21:07:43 +0100

    generator update output and handle github missing v6 support for dns servers

--- generator/generate.py
+++ generator/generate.py
@@ -18,6 +18,9 @@ def resolve(qname, rdtype):
         return ans
     except resolver.NoAnswer:
         return False
+    except resolver.NoNameservers:
+        print("Nameserver for %s not found/reached" % qname)
+        return False
 
 
 class SiteGroup:
@@ -26,9 +29,9 @@ class SiteGroup:
     list = []
 
     def handlesitefile(self, file):
-        print("Checking file: %s" % file)
+        print("::debug:: Checking file: %s" % file)
         site = file.stem
-        print("  Checking site: %s" % site)
+        print("::notice:: Checking site: %s" % site)
         with file.open() as f:
             data = json.load(f)
             sdata = {}
@@ -36,16 +39,19 @@ class SiteGroup:
             sdata["url"] = "%s://%s/" % (data["protocol"], site)
             sdata["description"] = data["description"]
             sdata["protocol"] = data["protocol"]
-            # TODO validate A and AAAA records
             rr_a = resolve(site, rdatatype.A)
+            wfile = "::warning file=%s" % file
             if rr_a:
                 print(
-                    "::warning,title=Has A::%s Got unexpected answer for A %s"
-                    % (site, rr_a)
+                    "%s,title=Has A:: %s Got unexpected answer for A '%s'"
+                    % (wfile, site, rr_a)
                 )
                 return
             rr_aaaa = resolve(site, rdatatype.AAAA)
-            print("::notice,title=Has AAAA::%s : %s" % (site, rr_aaaa))
+            if not rr_aaaa:
+                print("%s,title=Missing AAAA:: %s" % (wfile, site))
+                return
+            print("::notice,title=Has AAAA:: %s : '%s'" % (site, rr_aaaa))
             print(sdata)
             self.list.append(sdata)
 
